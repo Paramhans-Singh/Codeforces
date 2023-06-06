cbbb05b1407e5d2bf5020ca9cb828da9229876db
PARAMHANS <paramsingh11387@gmail.com>
2023-06-06 18:32:54 +0530

    Problem1

---
+++ Prob4A.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Prob4A{
+public static void main (String[] args){
+Scanner s = new Scanner(System.in);
+int t = s.nextInt();
+if(t%2==0){
+    System.out.println("YES");
+}
+else{
+    System.out.println("NO");
+}
+}
+}
